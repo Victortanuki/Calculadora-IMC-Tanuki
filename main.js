6c3dc35e494358204be5ece266435985926e977a
VictorTanukiDev <105583154+Victortanuki@users.noreply.github.com>
2024-06-10 08:00:42 -0300

    Pagina que recebe as informações do usuário
    Por enquanto só está utilizando o peso e altura, mas o imc depende de idade e sexo também, então planejo futuramente implementar isso e melhorar o design

---
+++ main.js
@@ -0,0 +1,32 @@
+let imc;
+
+function calcular() {
+    let verifaltura = document.getElementById("altura")
+    let verifpeso = document.getElementById("peso")
+
+    if (verifpeso.value === "" || verifaltura.value === "") {
+        alert("Peso e/ou altura ausente(s)")
+        return
+    }
+
+    let peso = parseFloat(verifpeso.value)
+    let altura = parseFloat(verifaltura.value)
+
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        alert("Insira dados válidos!")
+        return
+    }
+
+    imc = peso / (altura ** 2)
+localStorage.setItem('imc', imc)
+    window.location.href = '../finish/finish.html'
+
+}
+
+
+
+
+
+
+
+    
